2f7c2c4f7df2895b1299f73de4cf875ea7e99f08
Himanshu Yadav <himanshuyadav_23se074@dtu.ac.in>
2024-10-06 21:12:54 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum{
     public static void main(String[] args){
         int a=5;
         int b=6;
-        System.out.println();
+        System.out.println("This sum is" + (a+b));
     }
 }
